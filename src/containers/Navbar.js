5f510f702ee220d5859196e7b3ad169e6fe7e289
Karafizi Arthur <artur0509@mail.ru>
2017-06-21 00:29:43 +0300

    Refactoring of the Navbar.js
    mapDispatchToProps function was added, instead of directly passing the dispatch function to the component
    Rendering some stuff (like buttons and icons) was moved to their own functions that return the UI
    Styles of the UI was moved to localStyles object, instead of directly applying them in props of UI components

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -18,10 +18,18 @@ import {CodepenIcon, GithubIcon} from '../helpers';
 import SignupModal from '../containers/SignupModal';
 import LoginModal from '../containers/LoginModal';
 
+const localStyles = {
+  greetingsUser: { position: 'relative', top: '-6px' },
+  authButtons: { marginTop: '6px' },
+  loginButton: { marginRight: '10px' },
+  logoImage: { width: '100px', marginTop: '6px' },
+  githubButtonLabel: { top: '-2px', paddingLeft: '2px' },
+  portfolioButtonLabel: { top: '-2px', paddingLeft: '5px' }
+};
+
 class Navbar extends Component {
 
   static propTypes = {
-    dispatch: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired
   };
 
@@ -34,91 +42,83 @@ class Navbar extends Component {
     isSignupModalOpen: false
   };
 
-  openLoginModal = () => {
-    this.setState({ isLoginModalOpen: true });
-  };
+  openLoginModal = () => this.setState({ isLoginModalOpen: true });
 
-  openSignupModal = () => {
-    this.setState({ isSignupModalOpen: true });
-  };
+  openSignupModal = () => this.setState({ isSignupModalOpen: true });
 
-  closeLoginModal = () => {
-    this.setState({ isLoginModalOpen: false });
-  };
+  closeLoginModal = () => this.setState({ isLoginModalOpen: false });
 
-  closeSignupModal = () => {
-    this.setState({ isSignupModalOpen: false });
-  };
+  closeSignupModal = () => this.setState({ isSignupModalOpen: false });
 
-  logout = () => {
-    this.props.dispatch(logoutFromFirebase());
-  };
+  logout = () => this.props.logoutFromFirebase();
+
+  goToFavorites = () => this.context.router.push('/favorites');
 
   renderLoggedMenu = (props) => (
       <div>
-        <span style={{ position: 'relative', top: '-6px' }}>Hi, { this.props.user.email }</span>
+        <span style={localStyles.greetingsUser}>Hi, { this.props.user.email }</span>
         <IconMenu
             {...props}
             iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
             targetOrigin={{horizontal: 'right', vertical: 'top'}}
             anchorOrigin={{horizontal: 'right', vertical: 'top'}}
         >
-          <MenuItem onTouchTap={() => this.context.router.push('/favorites')} primaryText="Favorite Movies" />
+          <MenuItem onTouchTap={this.goToFavorites} primaryText="Favorite Movies" />
           <MenuItem primaryText="Log out" onTouchTap={this.logout} />
         </IconMenu>
       </div>
   );
 
-  renderLoginSignupBtns = () => {
-    return (
-        <div style={{ marginTop: '6px' }}>
-          <RaisedButton label="Login" onClick={ this.openLoginModal } style={{ marginRight: '10px' }} />
+  renderAuthButtons = () => (
+        <div style={localStyles.authButtons}>
+          <RaisedButton label="Login" onClick={ this.openLoginModal } style={localStyles.loginButton} />
           <RaisedButton label="Signup" secondary={true} onClick={ this.openSignupModal } />
           <SignupModal isOpen={this.state.isSignupModalOpen} closeModal={this.closeSignupModal} />
           <LoginModal isOpen={this.state.isLoginModalOpen} closeModal={this.closeLoginModal} />
         </div>
-    )
-  };
+    );
+
+  renderCreditButtons = () => (
+      <div>
+        <FlatButton
+            label="Github"
+            href="https://github.com/KarafiziArtur/the-movie-db-redux"
+            target="_blank"
+            icon={<GithubIcon viewBox="0 0 20 20" />}
+            labelStyle={localStyles.githubButtonLabel}
+        />
+        <FlatButton
+            label="Portfolio"
+            href="https://codepen.io/KarafiziArtur/full/gPxMqX"
+            target="_blank"
+            icon={<CodepenIcon viewBox="0 0 130 130" />}
+            labelStyle={localStyles.portfolioButtonLabel}
+        />
+      </div>
+  );
+
+  renderLogo = () => (
+      <Link to="/">
+        <img src={logo} className="App-logo" alt="logo" style={localStyles.logoImage} />
+      </Link>
+  );
 
   render() {
     return (
         <div className="Navbar">
           <AppBar
               className="Navbar__appbar"
-              iconElementLeft={
-                <Link to="/">
-                  <img src={logo} className="App-logo" alt="logo" style={{ width: '100px', marginTop: '6px' }} />
-                </Link>
-              }
-              title={
-                <div>
-                  <FlatButton label="Github"
-                              href="https://github.com/KarafiziArtur/the-movie-db-redux"
-                              target="_blank"
-                              icon={<GithubIcon viewBox="0 0 20 20" />}
-                              labelStyle={{ top: '-2px', paddingLeft: '2px' }}
-                  />
-                  <FlatButton label="Portfolio"
-                              href="https://codepen.io/KarafiziArtur/full/gPxMqX"
-                              target="_blank"
-                              icon={<CodepenIcon viewBox="0 0 130 130" />}
-                              labelStyle={{ top: '-2px', paddingLeft: '5px' }}
-                  />
-                </div>
-
-              }
-              iconElementRight={ this.props.user.isLoggedIn ? this.renderLoggedMenu() : this.renderLoginSignupBtns() }
+              iconElementLeft={this.renderLogo()}
+              title={this.renderCreditButtons()}
+              iconElementRight={ this.props.user.isLoggedIn ? this.renderLoggedMenu() : this.renderAuthButtons() }
           />
-
         </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user
-  };
-};
+const mapStateToProps = (state) => ({ user: state.user });
 
-export default connect(mapStateToProps)(Navbar);
+const mapDispatchToProps = (dispatch) => ({ logoutFromFirebase: () => dispatch(logoutFromFirebase()) });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
