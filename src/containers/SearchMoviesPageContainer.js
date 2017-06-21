930d073fd4f32a65cfc193861d71830e47cd10a9
Karafizi Arthur <artur0509@mail.ru>
2017-06-21 20:10:50 +0300

    Refactor SearchMoviesPageContainer.js
    mapDispatchToProps function was added, instead of directly passing the dispatch function to the component
    Styles moved to the localStyles object beyond the class of the component

--- src/containers/SearchMoviesPageContainer.js
+++ src/containers/SearchMoviesPageContainer.js
@@ -5,13 +5,16 @@ import { clearMovies, searchMovies } from '../actions/moviesActions';
 
 import MoviesList from '../components/MoviesList';
 
+const localStyles = {
+  noMoviesText: { textAlign: 'center', marginTop: '20px' }
+};
+
 class SearchMoviesPageContainer extends Component {
 
   static propsTypes = {
     ownProps: PropTypes.object.isRequired,
     movies: PropTypes.object.isRequired,
-    genres: PropTypes.array.isRequired,
-    dispatch: PropTypes.func.isRequired
+    genres: PropTypes.array.isRequired
   };
 
   componentDidMount() {
@@ -21,58 +24,62 @@ class SearchMoviesPageContainer extends Component {
   componentWillReceiveProps(nextProps) {
     const searchText = this.props.ownProps.location.query.query;
     const newSearchText = nextProps.ownProps.location.query.query;
+    
     if (searchText !== newSearchText) {
       this.loadMovies(newSearchText);
     }
   }
 
   componentWillUnmount() {
-    this.props.dispatch(clearMovies());
+    this.props.clearMovies();
   }
 
   loadMovies = (newSearchText) => {
-    this.props.dispatch(clearMovies());
+    this.props.clearMovies();
 
     const searchText = newSearchText || this.props.ownProps.location.query.query;
 
     if (searchText) {
-      this.props.dispatch(searchMovies(searchText));
+      this.props.searchMovies(searchText);
     }
   };
 
-  loadMore = (callback) => {
+  loadMore = () => {
     const { page, total_pages } = this.props.movies;
     const searchText = this.props.ownProps.location.query.query;
 
     if (page < total_pages) {
       const nextPage = page + 1;
-      this.props.dispatch(callback(searchText, nextPage));
+      this.props.searchMovies(searchText, nextPage);
     }
   };
 
   render() {
 
     if (!this.props.movies.moviesList.length) {
-      return <div style={{ textAlign: 'center', marginTop: '20px' }}>There is no movies for this search query.</div>
+      return <div style={localStyles.noMoviesText}>There are no movies for this search query.</div>
     }
 
     return (
-        <MoviesList callback={searchMovies}
-                    loadMore={this.loadMore}
-                    movies={this.props.movies}
-                    genres={this.props.genres}
-                    title="Found Movies"
+        <MoviesList
+            loadMore={this.loadMore}
+            movies={this.props.movies}
+            genres={this.props.genres}
+            title="Found Movies"
         />
     );
   }
 }
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    movies: state.movies,
-    genres: state.genres,
-    ownProps
-  }
-};
+const mapStateToProps = (state, ownProps) => ({
+  movies: state.movies,
+  genres: state.genres,
+  ownProps
+});
 
-export default connect(mapStateToProps)(SearchMoviesPageContainer);
+const mapDispatchToProps = dispatch => ({
+  searchMovies: (searchText, nextPage) => dispatch(searchMovies(searchText, nextPage)),
+  clearMovies: () => dispatch(clearMovies())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchMoviesPageContainer);
