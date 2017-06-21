83e34bea69b307a67f56e83dabfaa813aaafb097
Karafizi Arthur <artur0509@mail.ru>
2017-06-21 18:43:52 +0300

    Refactoring of the MoviesList.js
    Remove callback for LoadMoreButton because of this functionality's refactoring

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -7,11 +7,10 @@ import LoadMoreButton from './LoadMoreButton';
 import MoviesItemContainer from '../containers/MoviesItemContainer';
 import './MoviesList.css';
 
-const MoviesList = ({callback, movies, loadMore, genres, title}) => {
+const MoviesList = ({ movies, loadMore, genres, title }) => {
+  
   if (movies.error) {
-    return (
-        <h2>{movies.error} <br/>{movies.errorStatus}</h2>
-    );
+    return <h2>{movies.error} <br/>{movies.errorStatus}</h2>;
   }
 
   return (
@@ -31,7 +30,6 @@ const MoviesList = ({callback, movies, loadMore, genres, title}) => {
         {
           loadMore &&
           <LoadMoreButton
-              callback={callback}
               page={movies.page}
               total_pages={movies.total_pages}
               loadMore={loadMore}
@@ -45,7 +43,6 @@ MoviesList.propTypes = {
   movies: PropTypes.object.isRequired,
   genres: PropTypes.array.isRequired,
   title: PropTypes.string.isRequired,
-  callback: PropTypes.func,
   loadMore: PropTypes.func
 };
 
